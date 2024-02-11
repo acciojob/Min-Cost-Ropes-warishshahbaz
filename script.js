fa4b1fe54ecc4371a97db8e44e748dc2ffefbaea
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-02-11 15:37:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function mincost(arr)
 { 
+	let pq = [];
+	 for (let i = 0; i < arr.length; i++) {
+	 	pq.push(arr[i]);
+	 }
+	pq.sort((a,b)=> b-a);
+	let res = 0;
+	while (pq.length >1) {
+		let first = pq.shift();
+		let sec = pq.shift();
+		res += first + sec ;
+		pq.push(first + sec);
+		pq.sort((a,b)=>b-a);
+	}
+	return res;
 //write your code here
 // return the min cost
   
